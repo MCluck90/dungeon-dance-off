5ec599c112e04767f21e9d4549849025106b92e9
Mike Cluck <mike-cluck@pluralsight.com>
2020-08-18 16:10:22 -0600

    Setup camera system

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -11,7 +11,7 @@ func _ready():
 	var screen_size = OS.get_screen_size(0)
 	var window_size = OS.get_window_size()
 	OS.set_window_position(screen_size * 0.5 - window_size * 0.5)
-
+	OS.vsync_enabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
