fb7ab43d4b318a794b427420191f486f08c45c62
Mike Cluck <mike-cluck@pluralsight.com>
2020-08-19 12:50:11 -0600

    Improve health and beat bars

--- Prefabs/HealthBar.gd
+++ Prefabs/HealthBar.gd
@@ -10,8 +10,13 @@ func change_health(delta):
 	health += delta
 	
 func set_hearts():
+	for child in get_children():
+		if child.is_in_group("sub-heart"):
+			remove_child(child)
+	
 	for i in range(0, health, 1):
 		var heart = $BaseHeart.duplicate()
+		heart.add_to_group("sub-heart")
 		heart.visible = true
 		heart.position.x = -16.0 * i
 		self.add_child(heart)
