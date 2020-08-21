25a4721d567e4ed5e456d7e2091aafc79b801b11
Mike Cluck <mike-cluck@pluralsight.com>
2020-08-20 21:11:45 -0600

    Add button for restarting a level

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,8 +1,10 @@
 extends "Startup.gd"
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		return get_tree().reload_current_scene()
 
 func _on_next_level(scene_path):
-	get_tree().change_scene(scene_path)
+	return get_tree().change_scene(scene_path)
