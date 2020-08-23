53540d8fb458e7c755393adbd8feaefdf0035dda
Mike Cluck <mike-cluck@pluralsight.com>
2020-08-22 18:46:58 -0600

    Fix bug in beat bar timing

--- Player/HUD/BeatBar.gd
+++ Player/HUD/BeatBar.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 signal power
 
+var hit = null
+var hit_timer = null
+
 func _ready():
 	pass
+	
+func _process(_delta):
+	if hit_timer != null && hit_timer.get_time_left() <= 0.0:
+		remove_child(hit)
+		hit = null
+		hit_timer = null
 
 func _on_SelectionBar_power_signal(power_level):
 	emit_signal("power", power_level)
@@ -12,11 +21,9 @@ func _on_SelectionBar_reset_signal():
 	$SelectionBar.emit_signal("reset_signal")
 
 func show_hit():
-	var hit = $SelectionBar.duplicate()
+	hit = $SelectionBar.duplicate()
 	hit.remove_child(hit.get_child(0))
 	hit.modulate.g = 0
 	hit.modulate.b = 0
 	add_child(hit)
-	var timer = get_tree().create_timer($SelectionBar/Timer.wait_time * 3)
-	yield(timer, "timeout")
-	remove_child(hit)
+	hit_timer = get_tree().create_timer($SelectionBar/Timer.wait_time * 3)
