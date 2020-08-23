967f50767b5586c4aab11628c2d0613e2fa0fdac
Mike Cluck <mike-cluck@pluralsight.com>
2020-08-22 22:11:31 -0600

    Increase resolution

--- Player/HUD/SelectionBar.gd
+++ Player/HUD/SelectionBar.gd
@@ -4,13 +4,13 @@ signal power_signal
 
 func _on_Timer_timeout():
 	position.x += 16
-	if position.x >= 160:
-		position.x = 16
+	if position.x >= 176:
+		position.x = 32
 	var power_level = 0
-	if position.x >= 64 && position.x <= 96:
+	if position.x >= 80 && position.x <= 112:
 		power_level = 1
-	if position.x >= 112 && position.x <= 128:
+	if position.x >= 128 && position.x <= 144:
 		power_level = 2
-	if position.x > 128:
+	if position.x > 144:
 		power_level = 3
 	emit_signal("power_signal", power_level)
